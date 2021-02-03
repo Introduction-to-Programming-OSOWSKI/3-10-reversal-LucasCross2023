94d84e48a23674d47b7fc886b13a6c0c09f850ef
LucasCross2023 <817750@mystma.org>
2021-02-03 16:26:32 +0000

    reverse try 3

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def reversal(w):
     
-    rever = w[len (w) - 1]
+    reverse = w[len (w) - 1]
     for i in range(2, len(w)+ 1):
-        rever = rever + w[len (w) - i]
+        reverse = reverse + w[len (w) - i]
     
-    return rever 
+    return reverse 
 
 print(reversal("potato"))
